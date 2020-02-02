3394a29c1613d1739dfeae1de1473b8d66b2dd13
itope84 <topephysics@gmail.com>
2020-02-02 20:16:02 +0100

    created task model

---
+++ lib/models/task.dart
@@ -0,0 +1,30 @@
+import 'package:intl/intl.dart';
+import 'package:remind_launcher/utils/enums/notice-type.dart';
+
+class Task {
+  NoticeType type;
+  String title;
+  String body;
+  bool done;
+  DateTime createdAt;
+
+  Map<String, dynamic> toJson() {
+    Map<NoticeType, String> types = {
+      NoticeType.NOTE: 'note',
+      NoticeType.PROJECT: 'project',
+      NoticeType.REMINDER: 'reminder'
+    };
+
+    Map<String, dynamic> task = {
+      'type': types[this.type],
+      'title': this.title,
+      'body': this.body,
+      'done': this.done ? 1 : 0,
+      'created_at': DateFormat('y-M-d j:m:s'),
+    };
+
+    return task;
+  }
+
+  // Task.fromJson(Map<String, >)
+}
