24198bf2c3726cce2cec481855e6d9646241a961
itope84 <topephysics@gmail.com>
2020-02-02 11:35:18 +0100

    setting up

---
+++ lib/screens/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LauncherHome extends StatefulWidget {
+  @override
+  _LauncherHomeState createState() => _LauncherHomeState();
+}
+
+class _LauncherHomeState extends State<LauncherHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Hello"),
+      ),
+    );
+  }
+}
---
+++ lib/theme/style.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+TextStyle f18 = TextStyle(fontSize: 18.0);
+TextStyle f18Bold = f18.copyWith(fontWeight: FontWeight.bold);
