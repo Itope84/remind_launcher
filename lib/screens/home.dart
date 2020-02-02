ef920a6f24a2a61f1e7a6e0aea56e432c8fa0c72
itope84 <topephysics@gmail.com>
2020-02-02 22:04:07 +0100

    finished Addtasks design

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:remind_launcher/bloc/index.dart';
+import 'package:remind_launcher/models/task.dart';
+import 'package:remind_launcher/screens/add-task.dart';
 import 'package:remind_launcher/screens/drawer.dart';
 import 'package:remind_launcher/utils/enums/notice-type.dart';
+import 'package:remind_launcher/widgets/custom-bottom-modal.dart';
 
 class LauncherHome extends StatefulWidget {
   @override
@@ -14,6 +17,8 @@ class LauncherHome extends StatefulWidget {
 class _LauncherHomeState extends State<LauncherHome> {
   String _time = DateFormat('kk:mm').format(DateTime.now());
   String _date = DateFormat.MMMMEEEEd().format(DateTime.now());
+
+  bool _showBottomModal = false;
 
   // bloc
   MainBloc _bloc;
@@ -98,6 +103,96 @@ class _LauncherHomeState extends State<LauncherHome> {
     );
   }
 
+  Widget _taskListWidget() {
+    return Positioned(
+        bottom: 10.0,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 25.0),
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 255, 255, 0.5),
+                  borderRadius: BorderRadius.circular(30.0)),
+              width: MediaQuery.of(context).size.width - 20.0,
+              height: MediaQuery.of(context).size.height - 170,
+              padding: EdgeInsets.only(
+                left: 30,
+                right: 30,
+                top: 30,
+                bottom: 100.0,
+              ),
+              // TODO: Change to listview.builder
+              child: ListView(
+                children: <Widget>[
+                  _buildRemindItem(NoticeType.NOTE, 'DINNER',
+                      '2 wraps of Amala, Egusi soup'),
+                  // hr
+                  Divider(
+                    color: Color.fromRGBO(114, 96, 96, 0.76),
+                    indent: 30.0,
+                    thickness: 2.0,
+                    height: 40.0,
+                  ),
+                  _buildRemindItem(NoticeType.REMINDER, 'REMINDER',
+                      'Finish writing Personal statement'),
+
+                  Divider(
+                    color: Color.fromRGBO(114, 96, 96, 0.76),
+                    indent: 30.0,
+                    thickness: 2.0,
+                    height: 40.0,
+                  ),
+
+                  _buildRemindItem(NoticeType.PROJECT, 'PROJECT',
+                      'Build Personal Portfolio Website'),
+
+                  Divider(
+                    color: Color.fromRGBO(114, 96, 96, 0.76),
+                    indent: 30.0,
+                    thickness: 2.0,
+                    height: 40.0,
+                  ),
+
+                  _buildRemindItem(NoticeType.NOTE, 'DINNER',
+                      '2 wraps of Amala, Egusi soup'),
+                  // hr
+                  Divider(
+                    color: Color.fromRGBO(114, 96, 96, 0.76),
+                    indent: 30.0,
+                    thickness: 2.0,
+                    height: 40.0,
+                  ),
+                  _buildRemindItem(NoticeType.REMINDER, 'REMINDER',
+                      'Finish writing Personal statement'),
+
+                  Divider(
+                    color: Color.fromRGBO(114, 96, 96, 0.76),
+                    indent: 30.0,
+                    thickness: 2.0,
+                    height: 40.0,
+                  ),
+
+                  _buildRemindItem(NoticeType.PROJECT, 'PROJECT',
+                      'Build Personal Portfolio Website'),
+                ],
+              ),
+            ),
+            Positioned(
+              child: FloatingActionButton(
+                mini: true,
+                heroTag: 'addTask',
+                child: Icon(Icons.add),
+                onPressed: () {
+                  createNewTask();
+                },
+              ),
+              top: 0,
+              left: (MediaQuery.of(context).size.width / 2) - 25,
+            ),
+          ],
+        ));
+  }
+
   @override
   void initState() {
     // Get installed apps
@@ -113,6 +208,12 @@ class _LauncherHomeState extends State<LauncherHome> {
     );
 
     super.initState();
+  }
+
+  Future<void> createNewTask() async {
+    setState(() {
+      _showBottomModal = true;
+    });
   }
 
   @override
@@ -156,78 +257,7 @@ class _LauncherHomeState extends State<LauncherHome> {
                     ],
                   ),
                 ),
-                Positioned(
-                  bottom: 10.0,
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.0),
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(255, 255, 255, 0.5),
-                        borderRadius: BorderRadius.circular(30.0)),
-                    width: MediaQuery.of(context).size.width - 20.0,
-                    height: MediaQuery.of(context).size.height - 160,
-                    padding: EdgeInsets.only(
-                      left: 30,
-                      right: 30,
-                      top: 30,
-                      bottom: 100.0,
-                    ),
-                    // TODO: Change to listview.builder
-                    child: ListView(
-                      children: <Widget>[
-                        _buildRemindItem(NoticeType.NOTE, 'DINNER',
-                            '2 wraps of Amala, Egusi soup'),
-                        // hr
-                        Divider(
-                          color: Color.fromRGBO(114, 96, 96, 0.76),
-                          indent: 30.0,
-                          thickness: 2.0,
-                          height: 40.0,
-                        ),
-                        _buildRemindItem(NoticeType.REMINDER, 'REMINDER',
-                            'Finish writing Personal statement'),
-
-                        Divider(
-                          color: Color.fromRGBO(114, 96, 96, 0.76),
-                          indent: 30.0,
-                          thickness: 2.0,
-                          height: 40.0,
-                        ),
-
-                        _buildRemindItem(NoticeType.PROJECT, 'PROJECT',
-                            'Build Personal Portfolio Website'),
-
-                        Divider(
-                          color: Color.fromRGBO(114, 96, 96, 0.76),
-                          indent: 30.0,
-                          thickness: 2.0,
-                          height: 40.0,
-                        ),
-
-                        _buildRemindItem(NoticeType.NOTE, 'DINNER',
-                            '2 wraps of Amala, Egusi soup'),
-                        // hr
-                        Divider(
-                          color: Color.fromRGBO(114, 96, 96, 0.76),
-                          indent: 30.0,
-                          thickness: 2.0,
-                          height: 40.0,
-                        ),
-                        _buildRemindItem(NoticeType.REMINDER, 'REMINDER',
-                            'Finish writing Personal statement'),
-
-                        Divider(
-                          color: Color.fromRGBO(114, 96, 96, 0.76),
-                          indent: 30.0,
-                          thickness: 2.0,
-                          height: 40.0,
-                        ),
-
-                        _buildRemindItem(NoticeType.PROJECT, 'PROJECT',
-                            'Build Personal Portfolio Website'),
-                      ],
-                    ),
-                  ),
-                ),
+                _taskListWidget(),
                 Positioned(
                   bottom: 10.0,
                   child: Container(
@@ -285,6 +315,25 @@ class _LauncherHomeState extends State<LauncherHome> {
                       ],
                     ),
                   ),
+                ),
+                Positioned.fill(
+                  child: _showBottomModal
+                      ? CustomBottomModal(
+                          onClose: () {
+                            setState(() {
+                              _showBottomModal = false;
+                            });
+                          },
+                          child: AddTask(
+                            onSaveOrClose: (Task task) {
+                              setState(() {
+                                _bloc.addTask(task);
+                                _showBottomModal = false;
+                              });
+                            },
+                          ),
+                        )
+                      : Container(),
                 ),
               ],
             ),
